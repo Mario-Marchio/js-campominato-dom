01d0fe9c4d0740db8a89fc90e0ac85a3c3bb6d0f
Mario-Marchio <mariomarchio1610@gmail.com>
2023-06-08 20:06:42 +0200

    added 16 bombs

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-const createCell = () => {
-    const cell = document.createElement(`div`);
-    cell.className = `cell`;
-
-    return cell;
-}
-
 const button = document.getElementById(`play`);
 const table = document.getElementById(`table`);
 
@@ -13,6 +6,30 @@ const cols = 10;
 const allCells = rows * cols;
 
 let totalScore = 0;
+
+const createCell = () => {
+    const cell = document.createElement(`div`);
+    cell.className = `cell`;
+
+    return cell;
+}
+
+function randomNumber (){
+    const bombs = [];
+    for(let i =0; i < 16; i++ ){
+        let bomb;
+        do{
+            bomb = Math.floor(Math.random() * allCells + 1);
+        }while (bombs.includes(randomNumber));
+
+        bombs.push(bomb);
+    }
+    return bombs
+}
+
+const bombs = randomNumber();
+console.log(bombs)
+
 
 button.addEventListener(`click`, ()=>{
     
@@ -23,9 +40,10 @@ button.addEventListener(`click`, ()=>{
         cell.addEventListener(`click` , ()=>{
             cell.classList.add(`clicked`);
             console.log(cell.innerText);
-             ++totalScore;
+            ++totalScore;
             console.log(totalScore);
         })
         table.appendChild(cell);
     }
+
 });
